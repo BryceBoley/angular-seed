fc12dcf92072d9412002319af74f07c698df343c
Eric Jonese <ericnatejones@gmail.com>
2015-02-05 15:39:14 -0700

    Hooked up restangular

---
+++ app/events/events.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.events', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/events', {
+            templateUrl: 'events/events.html',
+            controller: 'EventsCtrl'
+        });
+    }])
+
+    .controller('EventsCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
+        Restangular.all('events').getList().then(function (data) {
+            $scope.events = data;
+        });
+    }]);
