e7ae89b891ec7f63f27b6c07b914daedd3c41a71
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-10 15:46:00 -0700

    changed gulpfile to port 8000 -- for frontend server

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('webserver', function() {
         .pipe(webserver({
             livereload: true,
             open: true,
-            port: 8002
+            port: 8000
             }));
 });
 
