652351a632963f289301084cffb973d69ae033d4
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-17 14:16:46 -0700

    changed css on members page and changed edit_members span to form

--- app/add_member/addMember.js
+++ app/add_member/addMember.js
@@ -14,7 +14,7 @@ angular.module('myApp.addMember', ['ngRoute'])
         // Add a new event, alert the user when it's been created or when there was a problem.
         $scope.addMember = function () {
             Restangular.all('members/').customPOST($scope.member).then(function () {
-                    alert("Your member was successfully added");
+                    //alert("Your member was successfully added");
                     $location.path('/members');
                 },
                 function () {
