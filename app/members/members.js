1e2f02df40e52385e968ebb9b17f157fae120d0b
Jonathan Wood <jonathankimballwood@gmail.com>
2015-02-17 18:22:20 -0700

    got some crazy merge stuff

--- app/members/members.js
+++ app/members/members.js
@@ -13,4 +13,27 @@ angular.module('myApp.members', ['ngRoute'])
         Restangular.all('members').getList().then(function (data) {
             $scope.members = data;
         });
-    }]);
+    }])
+
+    .filter('cut', function () {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || ' …');
+        };
+    });
+
+
+
